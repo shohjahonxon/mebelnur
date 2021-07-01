58e59a2743f186e85f0b30771d8e6894927e109a
shohjahonxon <81904306+shohjahonxon@users.noreply.github.com>
2021-07-01 14:26:54 +0500

    uzzagrdi

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+}); 
 
-    window.addEventListener('scroll', function(){
-        const header = document.querySelector('header');
-        header.classList.toggle("sticky", window.scrollY > 0);
-    }); 
-    
-    function menuToggle(){
-        const menuToggle = document.querySelector('menuToggle');
-        menuToggle.classList.toggle('active');
+function menuToggle(){
+    const menuToggle = document.querySelector('menuToggle');
+    menuToggle.classList.toggle('active');
 }  
 
-
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%" ;
+    document.getElementById("mySidenav").style.width = "100%";
   }
   
   function closeNav() {
