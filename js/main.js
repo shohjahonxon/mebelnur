336feae9d0b725b4e12bd50996b70b58c3bf2f8b
shohjahonxon <81904306+shohjahonxon@users.noreply.github.com>
2021-07-01 12:35:22 +0500

    mebelnur

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+    window.addEventListener('scroll', function(){
+        const header = document.querySelector('header');
+        header.classList.toggle("sticky", window.scrollY > 0);
+    }); 
+    
+    function menuToggle(){
+        const menuToggle = document.querySelector('menuToggle');
+        menuToggle.classList.toggle('active');
+}  
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%" ;
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
